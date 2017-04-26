1b9cf07912b6b8e13004e2101328c52e2fbee368
MarcinBalut <marcinmb13@tlen.pl>
2017-04-26 10:58:05 +0200

    only one test for only one function created :)

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,21 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
-
+    it('should return false if the argument is type of string', function ()    {
+        expect(app.reverseNumber('asa')).to.eql(false);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
-
+    it('should return ole', function ()    {
+        expect(app.returnOnlyLetter('1o2l3e4')).to.eql('ole');
+    });
 });
 
 describe('isEmail', function ()
 {
-
+    it('should return true for email: tom.chr@asd.com', function ()    {
+        expect(app.isEmail('tom.chr@asd.com')).to.eql(true);
+    });
 });
